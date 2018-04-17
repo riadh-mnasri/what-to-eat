85bf330bd5df6d3466fe8968685ba2a2346a2df5
Riadh MNASRI <riadh.mnasri@gmail.com>
2018-04-18 00:09:09 +0200

    FindById and Remove

--- src/main/java/com/wehightech/whattoeat/repository/RecipeRepository.java
+++ src/main/java/com/wehightech/whattoeat/repository/RecipeRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class RecipeRepository {
@@ -24,6 +25,10 @@ public class RecipeRepository {
 
     public List<Recipe> getAllRecipes(){
         return recipes;
+    }
+
+    public Optional<Recipe> findRecipeById(Integer idRecipe){
+        return recipes.stream().filter( r -> r.getId().equals(idRecipe)).findFirst();
     }
 
     public Integer addRecipe(Recipe recipe){
--- src/test/java/com/wehightech/whattoeat/repository/RecipeRepositoryTest.java
+++ src/test/java/com/wehightech/whattoeat/repository/RecipeRepositoryTest.java
@@ -38,4 +38,28 @@ public class RecipeRepositoryTest {
             new Ingredient("ingredient2")));
         Assertions.assertThat(recipeRepository.addRecipe(anyRecipe)).isEqualTo(EXPECTED_ID_RECIPE_TEST);
     }
+
+    @Test
+    public void findRecipeById() {
+        // Given
+        Recipe anyRecipe = new Recipe(ID_RECIPE_TEST, "Any Recipe", "Anything eatable", Arrays.asList(new Ingredient("ingredient1"),
+            new Ingredient("ingredient2")));
+        // When
+        recipeRepository.addRecipe(anyRecipe);
+        // Then
+        Assertions.assertThat(recipeRepository.findRecipeById(anyRecipe.getId()).get()).isEqualTo(anyRecipe);
+
+    }
+
+    @Test
+    public void removeRecipe() {
+        // Given
+        Recipe anyRecipe = new Recipe(ID_RECIPE_TEST, "Any Recipe", "Anything eatable", Arrays.asList(new Ingredient("ingredient1"),
+            new Ingredient("ingredient2")));
+        // When
+        recipeRepository.addRecipe(anyRecipe);
+        recipeRepository.removeRecipe(anyRecipe);
+        // Then
+        Assertions.assertThat(recipeRepository.findRecipeById(anyRecipe.getId())).isEmpty();
+    }
 }
